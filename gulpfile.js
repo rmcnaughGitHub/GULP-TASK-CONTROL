196b08bb4ff0c78a4ef0e2a19fd298ca46a691a6
Raymond R-Mcnaught <raymond.richards-mcnaught@grey.com>
2016-07-05 12:57:23 -0400

    -Added watch and live reload for JS files. Hooray!!!!

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,15 @@ var gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
 	minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
 	rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
-	browserSync = require('browser-sync').create();
-	//log = util.log;
+	browserSync = require('browser-sync').create(),
+	uglify = require('gulp-uglify');
 
 
 //Sass
 // Because Browsersync only cares 
 // about your CSS when it's finished compiling
 // - make sure you call .stream() after gulp.dest
-gulp.task('sass', function () { 
-	//log("Generate CSS files " + (new Date()).toString()); 
+gulp.task('sass', function() {  
     gulp.src('sass/style.scss')
         .pipe(sass({includePaths: ['scss'], style: 'expanded' }))
         .pipe(autoprefixer("last 3 version","safari 5", "ie 8", "ie 9"))
@@ -25,6 +24,15 @@ gulp.task('sass', function () {
 		.pipe(browserSync.stream());
     gulp.watch("sass/style.scss").on('change', browserSync.reload);
 });
+
+//JS Watch and Compress
+gulp.task('JS', function(){
+	gulp.src('js/main.js')
+	.pipe(uglify)
+	.pipe(gulp.dest('js'));
+	gulp.watch("js/main.js").on('change', browserSync.reload);
+})
+
 
 //Browser Synch - Static
 // ***can use 'serve' where 'browser-sync' is used***
@@ -38,6 +46,6 @@ gulp.task('browser-sync', function() {
 });
 
 //Default tasks
-gulp.task('default', ['sass', 'browser-sync'], function () {  
+gulp.task('default', ['sass', 'browser-sync', 'JS'], function () {  
     gulp.watch("sass/style.scss", ['sass']);
 });
